ac4830fdded42db00999e2d82c88a9f25e5757f2
LemonSheldon <714665786@qq.com>
2018-11-25 13:52:12 +0800

    没有安全检查的demo
    只检查了登陆状态以及upload/download/delete是否是该用户的文件夹

---
+++ hello.py
@@ -0,0 +1,149 @@
+import os
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    send_file,
+    session,
+)
+from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask.sessions import SessionInterface
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+SESSION_TYPE = 'redis'
+app.config.from_object(__name__)
+session_opts = {
+    'session.type': 'ext:memcached',
+    'session.url': '127.0.0.1:11211',
+    'session.data_dir': './cache',
+}
+app.secret_key = b'a5g8o1.8;5]85f5n2l5[\'65g6n-2d5d42l5d5rt'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+'sqlite:///'+os.path.join(basedir,'users.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return redirect('login')
+
+
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    try:
+        if 'user' not in session:
+            return redirect('login')
+        if request.method == 'POST':
+            if 'file' not in request.files:
+                return redirect(request.url)
+            file = request.files['file']
+            file_path = os.path.abspath(
+                os.path.join('../upload_files/' + session['user'],
+                             secure_filename(file.filename)))
+            allowed_path = os.path.abspath(
+                os.path.join('../upload_files/' + session['user']))
+            if allowed_path == file_path[:len(allowed_path)]:
+                while os.path.exists(file_path) and os.path.isfile(file_path):
+                    sp = os.path.splitext(file_path)
+                    file_path = sp[0] + ' - 副本' + sp[1]
+                file.save(file_path)
+    except Exception as e:
+        print(e.args)
+        return redirect('login')
+    return render_template('upload_file.html')
+
+
+@app.route('/list_file')
+def list_file():
+    try:
+        if 'user' not in session:
+            return redirect('login')
+        fl = os.listdir('../upload_files/' + session['user'])
+        return render_template('list_file.html', name=fl)
+    except Exception:
+        return redirect('login')
+
+
+@app.route('/download/<path:filename>')
+def download(filename):
+    try:
+        if 'user' not in session:
+            return redirect('login')
+        file_path = os.path.abspath((os.path.join(
+            '../upload_files/' + session['user'], secure_filename(filename))))
+        allowed_path = os.path.abspath(
+            os.path.join('../upload_files/' + session['user']))
+        if allowed_path == file_path[:len(allowed_path)]:
+            return send_file(file_path)
+    except Exception:
+        pass
+    return redirect('login')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        try:
+            user = request.form['user']
+            pwd = str(request.form['pwd'])
+            save_users = User.query.all()
+            for i in save_users:
+                if user == i.user and check_password_hash(i.pwd, pwd):
+                    session['user'] = user
+                    return redirect('list_file')
+        except Exception:
+            pass
+    return render_template('login.html')
+
+
+class User(db.Model):
+    user = db.Column(db.String(1024), primary_key=True)
+    pwd = db.Column(db.String(1024))
+
+    def __repr__(self):
+        return '<user %r>' % self.user
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if 'user' in session:
+        if request.method == 'POST':
+            db.session.add(
+                User(
+                    user=str(request.form['user']),
+                    pwd=generate_password_hash(request.form['pwd'])))
+            if request.form['pwd'] == request.form['re_pwd']:
+                db.session.commit()
+                os.mkdir('../upload_files/' + request.form['user'])
+            return redirect(request.url)
+        else:
+            return render_template('register.html')
+    else:
+        return redirect('login')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('login')
+
+
+@app.route('/delete/<path:filename>')
+def delete_file(filename):
+    if 'user' not in session:
+        return redirect('login')
+    file_path = os.path.abspath((os.path.join(
+        '../upload_files/' + session['user'], filename)))
+    allowed_path = os.path.abspath(
+        os.path.join('../upload_files/' + session['user']))
+    if allowed_path == file_path[:len(allowed_path)]:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+    return redirect('list_file')
