1ad850fe082363b55b8be4a802f13f3a07062794
Unknown <714665786@qq.com>
2018-11-27 01:54:34 +0800

    日志,手机显示
    修复手机上的错误显示,增加注册/登录的系统日志,注册成功或失败会发生跳转.

--- hello.py
+++ hello.py
@@ -12,6 +12,7 @@ from flask.sessions import SessionInterface
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import nacl.pwhash
+from hurry.filesize import size
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -24,9 +25,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-@app.route('/')
+@app.route('/pan')
 def index():
+    if 'user' in session:
+        return redirect('list_file')
     return redirect('login')
+
 
 @app.route('/upload_file', methods=['GET', 'POST'])
 def upload_file():
@@ -60,9 +64,18 @@ def list_file():
         if 'user' not in session:
             return redirect('login')
         fl = os.listdir('../upload_files/' + session['user'])
-        return render_template('list_file.html', name=fl, user=session['user'])
-    except Exception:
-        return redirect('login')
+        for i in range(len(fl)):
+            file_size = os.path.getsize(
+                os.path.abspath(
+                    os.path.join('../upload_files/' + session['user'],
+                                 str(fl[i]))))
+            file_name = fl[i]
+            fl[i] = {'size': str(size(file_size)), 'name': str(file_name)}
+        return render_template(
+            'list_file.html', files=fl, user=session['user'])
+    except Exception as e:
+        print(e.args)
+    return redirect('login')
 
 
 @app.route('/download/<path:filename>')
@@ -85,14 +98,19 @@ def download(filename):
 def login():
     if request.method == 'POST':
         try:
+            f = open('login_log.txt', mode='a+')
             user = request.form['user']
             pwd = str(request.form['pwd']).encode('utf-8')
             save_users = User.query.all()
             for i in save_users:
                 if user == i.user and nacl.pwhash.verify(i.pwd, pwd):
                     session['user'] = user
+                    f.write('User ' + str(user) + ' login at ' +
+                            str(datetime.datetime.now()) + ' successfully.\n')
                     return redirect('list_file')
         except Exception:
+            f.write('User ' + str(user) + ' login at ' +
+                    str(datetime.datetime.now()) + ' failed.\n')
             pass
     if 'user' in session:
         return render_template('login.html', user=session['user'])
@@ -111,23 +129,43 @@ class User(db.Model):
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     try:
-        if 'user' in session:
-            if request.method == 'POST':
+        if request.method == 'POST':
+            f = open('register_log.txt', mode='a+')
+            if 'user' in session:
                 db.session.add(
                     User(
                         user=str(request.form['user']),
-                        pwd=nacl.pwhash.str(request.form['pwd'].encode('utf-8'))))
+                        pwd=nacl.pwhash.str(
+                            request.form['pwd'].encode('utf-8'))))
                 pattern = re.compile("[A-Za-z0-9_-]+")
                 if request.form['pwd'] == request.form['re_pwd']:
-                    if pattern.fullmatch(str(request.form['user'])) is not None:
+                    if pattern.fullmatch(str(
+                            request.form['user'])) is not None:
                         db.session.commit()
                         os.mkdir('../upload_files/' + request.form['user'])
-                return redirect(request.url)
-            else:
-                return render_template('register.html')
+                        f.write('User ' + session['user'] +
+                                ' register an account named ' +
+                                request.form['user'] + ' at ' +
+                                str(datetime.datetime.now()) + ' successfully.\n')
+                        return render_template(
+                            'register_result.html', success='sucess')
+        else:
+            if 'user' in session:
+                return render_template('register.html', user=session['user'])
+            return render_template('register.html')
     except Exception as e:
-        print(e)
-    return redirect('login')
+        print(e.args)
+    if 'user' in session:
+        f.write('User ' + session['user'] +
+                    ' register an account named ' +
+                    request.form['user'] + ' at ' +
+                    str(datetime.datetime.now()) + ' failed.\n')
+        return render_template('register_result.html', user=session['user'])
+    else:
+        f.write('Someone do not have authority register an account named ' +
+                request.form['user'] + ' at ' + str(datetime.datetime.now()) +
+                ' failed.\n')
+        return render_template('register_result.html')
 
 
 @app.route('/logout')
