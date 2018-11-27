951d1d6bb15e6cb10e702c9dc272b4e40cc5a64d
Unknown <714665786@qq.com>
2018-11-28 02:44:17 +0800

    register by invite code

--- hello.py
+++ hello.py
@@ -13,6 +13,8 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 import nacl.pwhash
 from hurry.filesize import size
+import string
+import random
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -23,6 +25,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 'sqlite:///'+os.path.join(basedir,'users.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+def generate_invite_code(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 @app.route('/')
@@ -54,7 +60,8 @@ def upload_file():
                 allowed_path = os.path.abspath(
                     os.path.join('../upload_files/' + session['user']))
                 if allowed_path == file_path[:len(allowed_path)]:
-                    while os.path.exists(file_path) and os.path.isfile(file_path):
+                    while os.path.exists(file_path) and os.path.isfile(
+                            file_path):
                         sp = os.path.splitext(file_path)
                         file_path = sp[0] + ' - copy' + sp[1]
                     f.save(file_path)
@@ -75,13 +82,22 @@ def list_file():
                     os.path.join('../upload_files/' + session['user'],
                                  str(fl[i]))))
             file_name = str(fl[i])
-            sp = os.path.splitext(os.path.join('../upload_files/' + session['user'],
-                                 str(fl[i])))
+            sp = os.path.splitext(
+                os.path.join('../upload_files/' + session['user'], str(fl[i])))
             if len(sp[1]):
                 file_name = file_name[:-len(sp[1])]
-                fl[i] = {'size': str(size(file_size)), 'name': file_name, 'ext':str(sp[1][1:]),'filename':file_name+"."+str(sp[1][1:])}
+                fl[i] = {
+                    'size': str(size(file_size)),
+                    'name': file_name,
+                    'ext': str(sp[1][1:]),
+                    'filename': file_name + "." + str(sp[1][1:])
+                }
             else:
-                fl[i] = {'size': str(size(file_size)), 'name': file_name,'filename':file_name}
+                fl[i] = {
+                    'size': str(size(file_size)),
+                    'name': file_name,
+                    'filename': file_name
+                }
         return render_template(
             'list_file.html', files=fl, user=session['user'])
     except Exception as e:
@@ -136,12 +152,26 @@ class User(db.Model):
         return '<user %r>' % self.user
 
 
+class Invite_code(db.Model):
+    code = db.Column(db.String(16), primary_key=True)
+
+    def __repr__(self):
+        return '<user %r>' % self.code
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     try:
+        if 'user' in session:
+            return redirect('list_file')
         if request.method == 'POST':
             f = open('register_log.txt', mode='a+')
-            if 'user' in session:
+            icode = Invite_code.query.all()
+            icodel = []
+            for i in range(len(icode)):
+                icodel.append(icode[i].code)
+            have_code = request.form['invite_code'] in icodel
+            if have_code:
                 db.session.add(
                     User(
                         user=str(request.form['user']),
@@ -151,31 +181,30 @@ def register():
                 if request.form['pwd'] == request.form['re_pwd']:
                     if pattern.fullmatch(str(
                             request.form['user'])) is not None:
+                        db.session.delete(
+                            Invite_code.query.filter_by(
+                                code=request.form['invite_code']).first())
                         db.session.commit()
                         os.mkdir('../upload_files/' + request.form['user'])
-                        f.write('User ' + session['user'] +
-                                ' register an account named ' +
+                        f.write('Someone register an account named ' +
                                 request.form['user'] + ' at ' +
-                                str(datetime.datetime.now()) + ' successfully.\n')
+                                str(datetime.datetime.now()) +
+                                ' successfully.\n')
                         return render_template(
                             'register_result.html', success='sucess')
         else:
-            if 'user' in session:
-                return render_template('register.html', user=session['user'])
             return render_template('register.html')
     except Exception as e:
         print(e.args)
-    if 'user' in session:
-        f.write('User ' + session['user'] +
-                    ' register an account named ' +
-                    request.form['user'] + ' at ' +
-                    str(datetime.datetime.now()) + ' failed.\n')
-        return render_template('register_result.html', user=session['user'])
-    else:
+    if not have_code:
         f.write('Someone do not have authority register an account named ' +
                 request.form['user'] + ' at ' + str(datetime.datetime.now()) +
                 ' failed.\n')
-        return render_template('register_result.html')
+    else:
+        f.write('Someone have authority register an account named ' +
+                request.form['user'] + ' at ' + str(datetime.datetime.now()) +
+                ' failed.\n')
+    return render_template('register_result.html')
 
 
 @app.route('/logout')
@@ -198,4 +227,19 @@ def delete_file(filename):
                 os.remove(file_path)
         return redirect('list_file')
     except Exception:
-        return redirect(request.url)
+        return redirect('login')
+
+
+@app.route('/invite', methods=['GET', 'POST'])
+def invite():
+    if 'user' not in session:
+        return redirect('login')
+    if session['user'] != 'lemon' and session['user'] != 'smcj':
+        return redirect('list_file')
+    if request.method == 'GET':
+        return render_template('invite.html', user=session['user'])
+    ivc = generate_invite_code(16)
+    db.session.add(Invite_code(code=ivc))
+    db.session.commit()
+    return render_template(
+        'invite.html', invite_code=ivc, user=session['user'])
