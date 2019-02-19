e668b92696b21db8590e679be65de1bf96cae231
Chijun Sima <simachijun@gmail.com>
2019-02-19 12:54:57 +0800

    fix pylint warning

--- src/shop.py
+++ src/shop.py
@@ -14,7 +14,7 @@ basedir="../upload_files/anyone/shop"
 
 def find(pattern, path):
     result = []
-    for root, dirs, files in os.walk(path):
+    for root, _, files in os.walk(path):
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 result.append(os.path.join(root, name))
