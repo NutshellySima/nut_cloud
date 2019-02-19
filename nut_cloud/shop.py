272d69541a5ba69fd87072b873b4f2f140418841
Chijun Sima <simachijun@gmail.com>
2019-02-20 00:59:18 +0800

    resolve conflict

--- nut_cloud/shop.py
+++ nut_cloud/shop.py
@@ -10,7 +10,6 @@ import os, fnmatch
 import markdown2
 
 bp = Blueprint('shop', __name__, url_prefix='/shop')
-basedir=os.path.join(current_app.config['PANFILE'],"anyone/shop")
 
 def find(pattern, path):
     result = []
@@ -119,7 +118,7 @@ def addpic():
         flash('No file part',category="error")
         return redirect(request.referrer)
     try:
-        file_path=os.path.abspath(find(str(request.form['id'])+'.*',basedir)[0])
+        file_path=os.path.abspath(find(str(request.form['id'])+'.*',os.path.join(current_app.config['PANFILE'],"anyone/shop"))[0])
         if os.path.isfile(file_path):
             os.remove(file_path)
     except IndexError:
@@ -128,14 +127,14 @@ def addpic():
     for file in files:
         split_file_name = os.path.splitext(file.filename)
         path = str(request.form['id']) + split_file_name[1]
-        path=os.path.join(basedir, path)
+        path=os.path.join(os.path.join(current_app.config['PANFILE'],"anyone/shop"), path)
         file.save(path)
     return redirect(url_for('shop.index'))
 
 @bp.route('/getpic/<int:idnum>')
 def getpic(idnum):
     try:
-        path=os.path.abspath(find(str(idnum)+'.*',basedir)[0])
+        path=os.path.abspath(find(str(idnum)+'.*',os.path.join(current_app.config['PANFILE'],"anyone/shop"))[0])
         return send_file(path, conditional=True)
     except IndexError:
         return 'Error',404
